c666443811b801df4bf03664bd4848d80aff21bf
Yun Kui Pan <yun.pan@pinchfinancial.com>
2024-02-05 00:43:46 -0500

    supported prioritized data

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,6 @@
-import { heapPop, heapPush, heapify } from "./index";
+import { heappop, heappush, heapify } from "./index";
 
-describe("heapify", () => {
+describe("heapq on simple data", () => {
   it("heapifies an array into a min heap correctly", () => {
     const arr = [5, 7, 9, 1, 3];
     heapify(arr);
@@ -9,49 +9,49 @@ describe("heapify", () => {
 
   it("pops out a smallest item correctly", () => {
     const heap = [1, 3, 9, 7, 5];
-    expect(heapPop(heap)).toBe(1);
-    expect(heapPop(heap)).toBe(3);
-    expect(heapPop(heap)).toBe(5);
-    expect(heapPop(heap)).toBe(7);
-    expect(heapPop(heap)).toBe(9);
+    expect(heappop(heap)).toBe(1);
+    expect(heappop(heap)).toBe(3);
+    expect(heappop(heap)).toBe(5);
+    expect(heappop(heap)).toBe(7);
+    expect(heappop(heap)).toBe(9);
     expect(heap.length).toBe(0);
   });
 
   it("pushes an item and maintains the min heap properly", () => {
     const heap: number[] = [];
-    heapPush(heap, 3);
-    heapPush(heap, 1);
-    heapPush(heap, 4);
-    heapPush(heap, 1);
-    heapPush(heap, 5);
-    heapPush(heap, 9);
-    heapPush(heap, 2);
-    heapPush(heap, 6);
+    heappush(heap, 3);
+    heappush(heap, 1);
+    heappush(heap, 4);
+    heappush(heap, 1);
+    heappush(heap, 5);
+    heappush(heap, 9);
+    heappush(heap, 2);
+    heappush(heap, 6);
 
     expect(heap).toEqual([1, 1, 2, 3, 5, 9, 4, 6]);
   });
 
   it("pushes and pops correctly - case 1", () => {
     const heap: number[] = [];
-    heapPush(heap, 3);
-    heapPush(heap, 1);
-    heapPush(heap, 4);
-    heapPush(heap, 1);
-    heapPush(heap, 5);
-    heapPush(heap, 9);
-    heapPush(heap, 2);
-    heapPush(heap, 6);
+    heappush(heap, 3);
+    heappush(heap, 1);
+    heappush(heap, 4);
+    heappush(heap, 1);
+    heappush(heap, 5);
+    heappush(heap, 9);
+    heappush(heap, 2);
+    heappush(heap, 6);
 
     expect(heap).toEqual([1, 1, 2, 3, 5, 9, 4, 6]);
 
-    expect(heapPop(heap)).toBe(1);
-    expect(heapPop(heap)).toBe(1);
-    expect(heapPop(heap)).toBe(2);
-    expect(heapPop(heap)).toBe(3);
-    expect(heapPop(heap)).toBe(4);
-    expect(heapPop(heap)).toBe(5);
-    expect(heapPop(heap)).toBe(6);
-    expect(heapPop(heap)).toBe(9);
+    expect(heappop(heap)).toBe(1);
+    expect(heappop(heap)).toBe(1);
+    expect(heappop(heap)).toBe(2);
+    expect(heappop(heap)).toBe(3);
+    expect(heappop(heap)).toBe(4);
+    expect(heappop(heap)).toBe(5);
+    expect(heappop(heap)).toBe(6);
+    expect(heappop(heap)).toBe(9);
     expect(heap.length).toBe(0);
   });
 
@@ -59,14 +59,14 @@ describe("heapify", () => {
     const src: number[] = [3, 4, 3, 1, 45, 7, 674, 6, 99, 0, 6, 5, 4];
     const heap: number[] = [];
 
-    src.forEach((n) => heapPush(heap, n));
+    src.forEach((n) => heappush(heap, n));
     expect(heap.length).toBe(src.length);
 
     // if pushed correctly, then will be popped correctly.
-    let prev = undefined;
+    let prev: any = undefined;
     let i;
     for (i = 0; i < src.length; i++) {
-      let item = heapPop(heap);
+      let item: any = heappop(heap);
       if (prev !== undefined) {
         expect(item).toBeGreaterThanOrEqual(prev);
       }
@@ -74,6 +74,152 @@ describe("heapify", () => {
     }
     expect(i).toBe(src.length);
     expect(prev).toBe(674);
+    expect(heap.length).toBe(0);
+  });
+});
+
+describe("heapq on prioritized data", () => {
+  it("heapifies an array into a min heap correctly", () => {
+    const arr: [number, string][] = [
+      [5, "cherry"],
+      [7, "donut"],
+      [9, "egg"],
+      [1, "apple"],
+      [3, "banana"],
+    ];
+    heapify(arr);
+    expect(arr).toEqual([
+      [1, "apple"],
+      [3, "banana"],
+      [9, "egg"],
+      [7, "donut"],
+      [5, "cherry"],
+    ]);
+  });
+
+  it("pops out a smallest item correctly", () => {
+    const heap: [number, string][] = [
+      [1, "apple"],
+      [3, "banana"],
+      [9, "egg"],
+      [7, "donut"],
+      [5, "cherry"],
+    ];
+
+    expect(heappop(heap)).toEqual([1, "apple"]);
+    expect(heappop(heap)).toEqual([3, "banana"]);
+    expect(heappop(heap)).toEqual([5, "cherry"]);
+    expect(heappop(heap)).toEqual([7, "donut"]);
+    expect(heappop(heap)).toEqual([9, "egg"]);
+    expect(heap.length).toBe(0);
+  });
+
+  it("pushes an item and maintains the min heap properly", () => {
+    const heap: [number, string][] = [];
+    heappush(heap, [3, "cherry"]);
+    heappush(heap, [1, "apple"]);
+    heappush(heap, [4, "donut"]);
+    heappush(heap, [1, "apple"]);
+    heappush(heap, [5, "egg"]);
+    heappush(heap, [9, "apple-pie"]);
+    heappush(heap, [2, "banana"]);
+    heappush(heap, [6, "fries"]);
+
+    expect(heap).toEqual([
+      [1, "apple"],
+      [1, "apple"],
+      [2, "banana"],
+      [3, "cherry"],
+      [5, "egg"],
+      [9, "apple-pie"],
+      [4, "donut"],
+      [6, "fries"],
+    ]);
+  });
+
+  it("pushes and pops correctly - case 1", () => {
+    const heap: [number, string][] = [];
+    heappush(heap, [3, "cherry"]);
+    heappush(heap, [1, "apple"]);
+    heappush(heap, [4, "donut"]);
+    heappush(heap, [1, "apple"]);
+    heappush(heap, [5, "egg"]);
+    heappush(heap, [9, "apple-pie"]);
+    heappush(heap, [2, "banana"]);
+    heappush(heap, [6, "fries"]);
+
+    expect(heap).toEqual([
+      [1, "apple"],
+      [1, "apple"],
+      [2, "banana"],
+      [3, "cherry"],
+      [5, "egg"],
+      [9, "apple-pie"],
+      [4, "donut"],
+      [6, "fries"],
+    ]);
+
+    expect(heappop(heap)).toEqual([1, "apple"]);
+    expect(heappop(heap)).toEqual([1, "apple"]);
+    expect(heappop(heap)).toEqual([2, "banana"]);
+    expect(heappop(heap)).toEqual([3, "cherry"]);
+    expect(heappop(heap)).toEqual([4, "donut"]);
+    expect(heappop(heap)).toEqual([5, "egg"]);
+    expect(heappop(heap)).toEqual([6, "fries"]);
+    expect(heappop(heap)).toEqual([9, "apple-pie"]);
+    expect(heap.length).toBe(0);
+  });
+
+  it("pushes correctly", () => {
+    const src: [number, number][] = [
+      [3, 300],
+      [4, 400],
+      [3, -300],
+      [1, 100],
+      [45, 4500],
+      [7, -700],
+      [674, 67400],
+      [6, 600],
+      [99, 9900],
+      [0, 0],
+      [6, 600],
+      [5, -500],
+      [4, 400],
+    ];
+    const heap: [number, number][] = [];
+
+    src.forEach((n) => heappush(heap, n));
+
+    expect(heap.length).toBe(src.length);
+
+    expect(heap).toEqual([
+      [0, 0],
+      [1, 100],
+      [3, -300],
+      [4, 400],
+      [3, 300],
+      [4, 400],
+      [674, 67400],
+      [6, 600],
+      [99, 9900],
+      [45, 4500],
+      [6, 600],
+      [7, -700],
+      [5, -500],
+    ]);
+
+    // if pushed correctly, then will be popped correctly.
+    let prev: [number, number] | undefined = undefined;
+    let i;
+    for (i = 0; i < src.length; i++) {
+      let item: [number, number] = heappop(heap) as [number, number];
+      if (prev !== undefined) {
+        expect(item[0]).toBeGreaterThanOrEqual(prev[0]);
+      }
+      prev = item;
+    }
+    expect(i).toBe(src.length);
+    expect((prev as [number, number])[1]).toBe(67400);
     expect(heap.length).toBe(0);
   });
 });
--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
-export const heapify = <T>(arr: T[]): void => {
+// Number is the priority, T is the actual data to be stored.
+export type PrioritizedItem<T = any> = [number, T];
+
+export type Item<T = any> = T | PrioritizedItem<T>;
+
+// Convert the given array into min heap, where data with lower priority value will get popped out first.
+export const heapify = <T>(arr: Item<T>[]): void => {
   if (!arr) {
     return;
   }
@@ -7,12 +13,13 @@ export const heapify = <T>(arr: T[]): void => {
   }
 };
 
-export const heapPush = <T>(arr: T[], item: T): void => {
+export const heappush = <T>(arr: Item<T>[], item: Item<T>): void => {
   arr.push(item);
   let i = arr.length - 1;
   let pi = Math.floor((i - 1) / 2);
   let temp;
-  while (pi >= 0 && arr[pi] > arr[i]) {
+  while (pi >= 0 && isSmaller(arr[i], arr[pi])) {
+    // min heap, pop smaller to the top
     temp = arr[pi];
     arr[pi] = arr[i];
     arr[i] = temp;
@@ -21,7 +28,7 @@ export const heapPush = <T>(arr: T[], item: T): void => {
   }
 };
 
-export const heapPop = <T>(arr: T[]): T => {
+export const heappop = <T>(arr: Item<T>[]): Item<T> => {
   const x = arr[0];
   arr[0] = arr[arr.length - 1];
   arr.pop();
@@ -30,20 +37,16 @@ export const heapPop = <T>(arr: T[]): T => {
   return x;
 };
 
-export const heapMerge = <T>(arr1: T[], arr2: T[]): T[] => {
-  return [];
-};
-
-const _heapifyDown = <T>(arr: T[], i: number): void => {
+const _heapifyDown = <T>(arr: Item<T>[], i: number): void => {
   let smallest = i;
   let c1 = i * 2 + 1;
   let c2 = i * 2 + 2;
   let temp;
 
-  if (c1 < arr.length && arr[c1] < arr[smallest]) {
+  if (c1 < arr.length && isSmaller(arr[c1], arr[smallest])) {
     smallest = c1;
   }
-  if (c2 < arr.length && arr[c2] < arr[smallest]) {
+  if (c2 < arr.length && isSmaller(arr[c2], arr[smallest])) {
     smallest = c2;
   }
   if (smallest !== i) {
@@ -53,4 +56,16 @@ const _heapifyDown = <T>(arr: T[], i: number): void => {
 
     _heapifyDown(arr, smallest);
   }
+};
+
+const isSmaller = <T>(item1: Item<T>, item2: Item<T>): boolean => {
+  if (Array.isArray(item1) !== Array.isArray(item2)) {
+    throw new Error("Item type inconsistent");
+  }
+
+  if (Array.isArray(item1) && Array.isArray(item2)) {
+    return item1[0] < item2[0]; // first element is the priority
+  }
+
+  return item1 < item2;
 };
